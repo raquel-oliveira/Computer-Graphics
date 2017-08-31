47d6be52ad173e0d68c9a3c751068c71931874bc
Raquel Lopes de Oliveira <oliveira.raquel.lopes@gmail.com>
2017-08-31 15:12:25 -0300

    Refactor code #10
    Implement intersection method in order to improve legibility and modularize code.
    Related with #25 and #8.

--- include/shader.h
+++ include/shader.h
@@ -8,6 +8,24 @@ class Shader {
   public:
     virtual Color3 find_color(Scene scene,const Ray& r_) const = 0;
 
+    bool intersect( Scene scene, const Ray &r_, Hit &hr ) const{
+      bool check = false;
+      Hit hr_prev; //hitRecords
+      //for(const auto & i : scene.getObjects()){
+      for (std::vector<Object*>::iterator i = scene.getObjects()->begin() ; i != scene.getObjects()->end(); i++){
+        if ((*i)->hit(r_, TMIN, TMAX, hr_prev)){
+          if (!check){ //First time hit
+            hr = hr_prev;
+          } else{
+            if(hr_prev.t < hr.t){ //Update if hit a closest object
+              hr = hr_prev;
+            }
+          }
+          check = true;
+        }
+      }
+    return check;
+    }
 };
 
 class Normal2RGB : public Shader{
@@ -15,24 +33,9 @@ class Normal2RGB : public Shader{
     Normal2RGB(){}
 
     Color3 find_color(Scene scene,const Ray& r_) const{
-      Hit hr, hr2; //hitRecords
-      int check = -1;
-      Vec3 n;
-      for (std::vector<Object*>::iterator it = scene.getObjects()->begin() ; it != scene.getObjects()->end(); it++){
-        if ((*it)->hit(r_, TMIN, TMAX, hr2)){
-          if (check == -1){
-            hr = hr2; // initialize t_prev
-            n = Vec3((hr.normal.x()+1), (hr.normal.y()+1), (hr.normal.z()+1)) * 0.5; //normal gives a number between -1 and 1, an we want between 0 and 1 (+1/2)
-          } else {
-            if (hr2.t < hr.t){
-              hr = hr2;
-              n = Vec3((hr.normal.x()+1), (hr.normal.y()+1), (hr.normal.z()+1)) * 0.5; //normal gives a number between -1 and 1, an we want between 0 and 1 (+1/2)
-            }
-          }
-          check = 0; // hit a object
-        }
-      }
-      if (check == 0){
+      Hit hr;
+      if(intersect(scene, r_, hr)){
+        Color3 n = Vec3((hr.normal.x()+1), (hr.normal.y()+1), (hr.normal.z()+1)) * 0.5; //normal gives a number between -1 and 1, an we want between 0 and 1 (+1/2)
         return (255*n);
       }
 
@@ -58,32 +61,18 @@ class Depth : public Shader{
       foreground_depth(cmax) {}
 
     Color3 find_color(Scene scene,const Ray& r_) const{
-      Hit hr_prev, hr;
-      int check = -1;
       float distance=-1;
-      for (std::vector<Object*>::iterator it = scene.getObjects()->begin() ; it != scene.getObjects()->end(); it++){
-        if ((*it)->hit(r_, TMIN, TMAX, hr_prev)){
-          if (check == -1){
-            hr = hr_prev; // initialize t_prev
-            distance = (hr.point - r_.get_origin()).length();
-            distance = std::max(distance, depth_min);
-            distance = std::min(distance, depth_max);
-          } else {
-            if (hr_prev.t < hr.t){
-              hr = hr_prev;
-              distance = (hr.point - r_.get_origin()).length();
-              distance = std::max(distance, depth_min);
-              distance = std::min(distance, depth_max);
-            }
-          }
-          check = 0; // hit a object
-        }
-      }
-      if (check == 0){
+      Hit hr;
+      if(intersect(scene, r_, hr)){
+        distance = (hr.point - r_.get_origin()).length();
+        distance = std::max(distance, depth_min);
+        distance = std::min(distance, depth_max);
+
         float k = (distance-depth_min)/(depth_max-depth_min); //normalize
         Color3 color = interp(foreground_depth, background_depth, k);
         return (255*color);
       }
+
       return 255*background_depth;
     }
 };
